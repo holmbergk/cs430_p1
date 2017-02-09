fc72e92b0b63583bab921659da8d268ab7b15fea
icecrusher42 <icecrusher42@users.noreply.github.com>
2017-02-09 01:13:59 -0600

    removed unneeded final variables

--- Warehouse.java
+++ Warehouse.java
@@ -3,15 +3,7 @@ import java.util.*;
 import java.io.*;
 public class Warehouse implements Serializable {
   private static final long serialVersionUID = 1L;
-  public static final int BOOK_NOT_FOUND  = 1;
-  public static final int BOOK_NOT_ISSUED  = 2;
-  public static final int BOOK_HAS_HOLD  = 3;
-  public static final int BOOK_ISSUED  = 4;
-  public static final int HOLD_PLACED  = 5;
-  public static final int NO_HOLD_FOUND  = 6;
-  public static final int OPERATION_COMPLETED= 7;
-  public static final int OPERATION_FAILED= 8;
-  public static final int NO_SUCH_MEMBER = 9;
+
   private Inventory inventory;
   private ClientList clientList;
   private SupplierList supplierList;
